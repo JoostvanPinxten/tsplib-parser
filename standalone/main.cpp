e57b16ea173c9be2d3dbeb5ed3016a97ef2be3f5
Joost van Pinxten <j.h.h.v.pinxten@tue.nl>
2016-03-17 22:42:00 +0100

    Added a note to the standalone main entry.

--- standalone/main.cpp
+++ standalone/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
 int main(int argc, char ** argv) {
+    std::cout << "Not implemented yet! Check out the integration tests for ran example usage." << std::endl;
     return 0;
 }
